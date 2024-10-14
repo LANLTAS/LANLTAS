77c09314e8eddff0867458aa547f19c4b921681f
Mohamed-hhu <Hassanhashimohamed@gymnasiumbreda.nl>
2024-10-14 11:28:21 +0200

    Add files via upload

---
+++ RuVSNi27oct2024.js
@@ -0,0 +1,3 @@
+alert("in order to watch it live, you need to have a Scratch account and have to be a scratcher")
+alert ("the information may be not accurate if the match is postponed or cancelled, be careful of it")
+alert ("the link to the livestream won't work earlier than 5 minutes before the match")
